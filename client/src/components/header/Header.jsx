84019c5f6b988a42fcc6a6d58fdc23b0e953a691
aliaalam <aalamali696@gmail.com>
2022-03-31 11:40:50 +0530

    initial commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <div className="headerImg"></div>
+      {/* <img
+        className="headerImg"
+        // src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        // src="https://3.bp.blogspot.com/-XNXmpb05Dp4/XAbvsOa1wcI/AAAAAAAABP8/nDxytauIDxYKZvt3xHk3vzGhX1MMaKR-wCHMYCw/s1600/wallpaper-1300x812-px-boat-church-forest-italy-lake.jpg"
+        alt=""
+      /> */}
+    </div>
+  );
+}
